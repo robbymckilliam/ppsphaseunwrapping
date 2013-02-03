458d50584b548703e60b58d610229c068eb28511
Vaughan Clarkson <v.clarkson@uq.edu.au>
2013-02-03 11:36:31 +1030

    Fixed script bug

--- code/runsim.sh
+++ code/runsim.sh
@@ -1,5 +1,5 @@
 #export JAVA_OPTS="-Xprof -server -Xms1g -Xmx1g"
 export JAVA_OPTS="-d64 -server -Xms1g -Xmx1g"
-#scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar simgaussian.scala
-scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar simgaussianfixedmu.scala
+scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar simgaussian.scala
+#scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar simgaussianfixedmu.scala
 #scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar simwrappedu.scala
